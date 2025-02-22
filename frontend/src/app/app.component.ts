8309ebc37692009adbd36e7f6ccc28fefa236375
vres271 <vres271@gmail.com>
2025-02-22 23:47:27 +0400

    Implement user management features with add and delete functionality, including error handling and styling updates

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface IError {
+  message: string[],
+  error: string,
+  statusCode: number
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,7 +17,9 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent implements OnInit {
   title = 'mft5-frontend';
   backendHealth = '';
-
+  users: any[] = [];
+  error: IError|null = null;
+  
   ngOnInit() {
     console.log('AppComponent initialized');
     fetch('api/health')
@@ -19,6 +27,47 @@ export class AppComponent implements OnInit {
       .then(text => {
         this.backendHealth = text;
       });
+
+    fetch('api/users')
+      .then(response => response.json())
+      .then(data => {
+        this.users = data;
+      });
+    
+  }
+
+  deleteUser(user: any) {
+    fetch(`api/users/${user.id}`, {method: 'DELETE'})
+    .then(() => {
+      this.users = this.users.filter(u => u.id !== user.id);
+    });
+  }
+
+  addUser(name: string, email: string, password: string) {
+    this.error = null;
+    fetch('api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name, email, password})
+    })
+    .then(response => response.json())
+    .then(res => {
+      if (res.error) {
+        this.errorHandler(res);
+        return;
+      }
+      this.users.push(res);
+    })
+;
+  }
+
+  errorHandler(error: IError) {
+    this.error = {
+      ...error,
+      message: error.message instanceof Array ? error.message : [error.message]
+    };
   }
 
 }
